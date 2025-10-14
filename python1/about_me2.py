9acab6ea9920d9b1a80d74f235b275927e2bcd13
hartigpy <yael.hartig@gmail.com>
2025-10-14 12:06:39 -0400

    using + to separate print arguments

--- python1/about_me2.py
+++ python1/about_me2.py
@@ -5,5 +5,14 @@ import sys
 
 my_name = sys.argv[1]
 my_fav_color = sys.argv[2]
+my_fav_activity = sys.argv[3]
+my_fav_animal = sys.argv[4]
 
-print("My name :", my_name, 'and', "my favorite color is", my_fav_color)
+# now uding + to separate print arguments
+print("My name: " + my_name) 
+print("My favorite color: " + my_fav_color)
+print("My favorite activity: " + my_fav_activity)
+print("Mz favorite animal: " + my_fav_animal)
+
+# when running this script it is necessary to add content in the command line
+# ./about_me2.py Yael Black Coding Dog
