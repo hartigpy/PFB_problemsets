88ac7fd511d39b4d4aef12b1644a23adcc773d02
hartigpy <yael.hartig@gmail.com>
2025-10-14 11:57:18 -0400

    tested sys.argv

---
+++ python1/about_me2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3 
+
+# now using the special built-in list sys.argv
+import sys
+
+my_name = sys.argv[1]
+my_fav_color = sys.argv[2]
+
+print("My name :", my_name, 'and', "my favorite color is", my_fav_color)
