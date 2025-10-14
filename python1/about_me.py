b3047ee94381a1e180ba22ac314e51aada689950
hartigpy <yael.hartig@gmail.com>
2025-10-14 11:56:36 -0400

    added activity and animal

--- python1/about_me.py
+++ python1/about_me.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3 
 
+# assigning stuff to a variable and printing the variable
 my_name = "Yael"
 print("My name:", my_name)
 
 my_fav_color = "Blue"
 print("My favourite color:", my_fav_color)
+
+my_fav_activity = "Coding"
+print("My favorite activity:", my_fav_activity)
+
+# again assigning something to a variable
+# different way of printing using f"" string formating
+my_fav_animal = "Dog"
+print(f"My favorite animal: {my_fav_animal}") 
