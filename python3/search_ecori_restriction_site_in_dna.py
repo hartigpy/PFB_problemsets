8e379865725fd89c06ed64755cf11d25bd419e23
hartigpy <yael.hartig@gmail.com>
2025-10-15 20:45:11 -0400

    added finding motif on reverse

--- python3/search_ecori_restriction_site_in_dna.py
+++ python3/search_ecori_restriction_site_in_dna.py
@@ -5,10 +5,18 @@ import sys
 dna = sys.argv[1]
 
 ### make DNA sequence uppercase
-dna_upper = dna.upper()
+dna_up = dna.upper()
 
 ### definition of EcoRI restriction site
 motif = "GAATTC"
 
-motif_pos = dna_upper.find(motif)
-print(f'found EcoRImotif in start-position: {motif_pos+1} and end-position: {motif_pos+len(motif)+1}') 
+### find motif and position on forward strand
+motif_pos_fw = dna_up.find(motif)
+print(f'found EcoRImotif on forward strand in start-position: {motif_pos_fw+1} and end-position: {motif_pos_fw+len(motif)}') 
+
+### find motif and position on reverse strand
+dna_comp = dna_up.replace("A", "t").replace("T", "a").replace("G", "c").replace("C", "g")
+dna_comp_up = dna_comp.upper()
+rev_dna_comp = dna_comp_up[::-1]
+motif_pos_rv = rev_dna_comp.find(motif)
+print(f'found EcoRImotif on reverse strand in start-position: {motif_pos_rv+1} and end-position: {motif_pos_rv+len(motif)}') 
