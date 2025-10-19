99edfdac976273b58023b49de035ae8e8aad77f6
hartigpy <yael.hartig@gmail.com>
2025-10-18 20:05:11 -0400

    reduced to just one while loop

--- python4/while_number_factorial.py
+++ python4/while_number_factorial.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3 
 
-number = 0
-factorial = 0
-while number < 11:
-    number = number + 1
-    factorial = number * (number - 1)
+### Write a while loop to calculate the factorial of 10
+
+number_list = [1,2,3,4,5,6,7,8,9,10] # creating a list with numbers from 1-10
+factorial = 1
+i = 0
+
+
+# Calculate factorial by multiplying all elements in the list
+while i < len(number_list):   
+    factorial = factorial * number_list[i]
+    i = i + 1
+    
 print(factorial)
