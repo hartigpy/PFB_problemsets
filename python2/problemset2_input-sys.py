388d2aefb1382657f35041a4f0b7abcc9055a761
hartigpy <yael.hartig@gmail.com>
2025-10-18 20:03:40 -0400

    added files in new directory

---
+++ python2/problemset2_input-sys.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3 
+import sys
+
+count = int(sys.argv[1])
+print(count,"is the tested number")
+
+if count > 0: # tests if the number is positive
+    print(count, "is positive") 
+    if count < 50: # tests if the number is below 50
+        if count % 2 == 0: # tests if the number is even
+            print(count, "is less than 50 and an even number")
+        else:
+            print(count, "is less than 50 and an odd number")
+    if count > 50: # tests if the number is above 50
+        if count % 3 == 0: # tests if the number is divisiable by 3
+            print(count, "is larger than 50 and is divisable by 3")   
+        else:
+            print(count, "is larger than 50 and is not divisiable by 3")    
+    if count == 50: # tests if the number is exactly 50
+        print(count, "is exactly 50")  
+elif count == 0: # tests if the number is exactly 0
+    print(count, "is 0")
+else:
+    print(count, "is negative")
