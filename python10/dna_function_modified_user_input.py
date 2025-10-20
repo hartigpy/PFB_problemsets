421a27de30fe1133bd132cdafa1b622bff62b0bb
hartigpy <yael.hartig@gmail.com>
2025-10-20 11:21:57 -0400

    added user input

---
+++ python10/dna_function_modified_user_input.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3 
+import sys
+
+### Create a function to format a string of DNA so that each line is no more than 60 nt lng
+
+## Read in variables and give user instructions on what to enter as input
+dna_string = ''
+try:
+    dna_string = sys.argv[1]
+    print("User provided DNA sequence:", dna_string)
+except:
+    print("Please provide a DNA sequence")
+
+width = ''
+try:
+    width = int(sys.argv[2])
+    print("User provided maximum length of line:", width)
+except:
+    print("Please provide maximum length of line")     
+
+## Create the function to return 60 nt chunks
+def dna_width_nt(dna):
+    dna_chunk = []
+    i = 0
+    dna = dna.replace("\n", "")
+    while i < len(dna):
+        dna_chunk.append(dna[i:i+width])
+        i += width
+        #print(i)
+    dna_chunk_string = '\n'.join(dna_chunk) # joining of list items in a string with \n
+    return(dna_chunk_string)
+
+## Call the function
+print(f'This is the provided DNA sequence split according to the maximum length:')
+print(f'{dna_width_nt(dna_string)}')
