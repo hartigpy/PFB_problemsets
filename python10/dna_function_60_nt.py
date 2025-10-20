3ad71e208326a8920a6772fd8fa87b8b0bfda854
hartigpy <yael.hartig@gmail.com>
2025-10-20 11:21:27 -0400

    removeed user input

--- python10/dna_function_60_nt.py
+++ python10/dna_function_60_nt.py
@@ -4,19 +4,13 @@ import sys
 ### Create a function to format a string of DNA so that each line is no more than 60 nt lng
 
 ## Read in variables and give user instructions on what to enter as input
-dna_string = ''
-try:
-    dna_string = sys.argv[1]
-    print("User provided DNA sequence:", dna_string)
-except:
-    print("Please provide a DNA sequence")
-
-width = ''
-try:
-    width = int(sys.argv[2])
-    print("User provided maximum length of line:", width)
-except:
-    print("Please provide maximum length of line")     
+dna_string = '''GATGGGATTGGGGTTTTCCCCTCCCATGTGCTCAAGACTGGCGCTAAAAGTTTTGAGCTTCTCAAAAGTCTAGAGCCACC
+GTCCAGGGAGCAGGTAGCTGCTGGGCTCCGGGGACACTTTGCGTTCGGGCTGGGAGCGTGCTTTCCACGACGGTGACACG
+CTTCCCTGGATTGGCAGCCAGACTGCCTTCCGGGTCACTGCCATGGAGGAGCCGCAGTCAGATCCTAGCGTCGAGCCCCC
+TCTGAGTCAGGAAACATTTTCAGACCTATGGAAACTACTTCCTGAAAACAACGTTCTGTCCCCCTTGCCGTCCCAAGCAA
+TGGATGATTTGATGCTGTCCCCGGACGATATTGAACAATGGTTCACTGAAGACCCAGGTCCAGATGAAGCTCCCAGAATG
+CCAGAGGCTGCTCCCCCCGTGGCCCCTGCACCAGCAGCTCCTACACCGGCGGCCCCTGCACCAGCCCCCTCCTGGCCCCT
+GTCATCTTCT'''    
 
 ## Create the function to return 60 nt chunks
 def dna_60_nt(dna):
@@ -24,12 +18,12 @@ def dna_60_nt(dna):
     i = 0
     dna = dna.replace("\n", "")
     while i < len(dna):
-        dna_chunk.append(dna[i:i+width])
-        i += width
+        dna_chunk.append(dna[i:i+60])
+        i += 60
         #print(i)
     dna_chunk_string = '\n'.join(dna_chunk) # joining of list items in a string with \n
     return(dna_chunk_string)
 
 ## Call the function
-print(f'This is the provided DNA sequence split according to the maximum length:')
+print(f'This is the provided DNA sequence split according to the maximum length of 60 nt:')
 print(f'{dna_60_nt(dna_string)}')
